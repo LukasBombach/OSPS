12232686ad209e2baa46f932b7e081e5746473da
Lukas Bombach <lukas.bombach@googlemail.com>
2019-12-16 12:38:44 +0100

    added a canvas element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Nav from "../components/nav";
+import { Stage, Layer, Rect, Text } from "react-konva";
 
 const Home = () => (
   <div>
@@ -9,32 +9,18 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <Nav />
-
     <div className="hero">
       <h1 className="title">Welcome to Next.js!</h1>
       <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
     </div>
+    <Stage width={800} height={600}>
+      <Layer>
+        <Text text="Some text on canvas" fontSize={15} />
+        <Rect x={20} y={50} width={100} height={100} fill="red" />
+      </Layer>
+    </Stage>
 
     <style jsx>{`
       .hero {
@@ -51,35 +37,6 @@ const Home = () => (
       .title,
       .description {
         text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
       }
     `}</style>
   </div>
